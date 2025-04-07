f67ea5b2a8c447c2fdc445bce38d92b6532ae6ff
OH JIN BO <wlsqh0825@hanmail.net>
2025-04-07 02:46:14 +0000

    hello

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ print(f"{s_number}단:")
 # print(number + "단:" + number + "단:" + number + "단:")
 # print(f"{number}단:", {number}단:, {number}단:")
 # number 값이 문자열이기 때문에 숫자로 변경해서 연산
- \number = int(s_number)
+number = int(s_number)
 result = number * 2
 print(result)
---
+++ test4.py
@@ -0,0 +1,6 @@
+# 함수 만들기
+def hello():
+    print("hello world!")
+
+# 함수 호출(실행)
+hello()
